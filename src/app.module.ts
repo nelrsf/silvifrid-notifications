ad5528c1a2847f89735b6a8a8d95e70a8997555d
nelrsf <nel.rsf@gmail.com>
2024-07-31 00:08:09 -0500

    fix dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotificationsModule } from './notifications/notifications.module';
+import { WhatsappNotifications } from './model/WhatsappNotifications';
 
 @Module({
   imports: [NotificationsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, WhatsappNotifications],
 })
 export class AppModule {}
